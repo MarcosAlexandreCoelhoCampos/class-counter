580dc96bf97e9dda18184a2c40b235545be49106
MarcosAlexandreCoelhoCampos <marcooscampos@hotmail.com>
2022-11-17 12:55:47 -0300

    Add todos os arquivos

--- infos.js
+++ infos.js
@@ -1,5 +1,6 @@
-let total_class_goal = 397;
-let class_goal_per_day = 8;
+// 408 - 217
+let total_class_goal = 191;
+let class_goal_per_day = 15;
 let total_study_days_per_week = 5;
 
 export { total_class_goal, class_goal_per_day, total_study_days_per_week };
--- total_classes_per_day.js
+++ total_classes_per_day.js
@@ -4,32 +4,23 @@ function create_new_record(day, classes = 0) {
   total_classes_per_day.push({ day, classes });
 }
 
-// Week 1
-create_new_record("09/12/2022", 44);
-create_new_record("09/13/2022", 20);
-create_new_record("09/14/2022", 9);
-create_new_record("09/15/2022", 10);
-create_new_record("09/16/2022", 11);
+// WEEK 1
+create_new_record("14/11/2022", 7);
+create_new_record("15/11/2022", 11);
+create_new_record("16/11/2022", 15);
+// create_new_record("17/11/2022");
+// create_new_record("18/11/2022");
 
-// Week 2
-create_new_record("09/19/2022", 6);
-create_new_record("09/20/2022", 12);
-create_new_record("09/21/2022", 9);
-create_new_record("09/22/2022", 4);
-create_new_record("09/23/2022", 9);
+// WEEK 2
+// create_new_record("21/11/2022");
+// create_new_record("22/11/2022");
+// create_new_record("23/11/2022");
+// create_new_record("24/11/2022");
+// create_new_record("25/11/2022");
 
-// Week 3
-create_new_record("09/26/2022");
-create_new_record("09/27/2022");
-create_new_record("09/28/2022");
-create_new_record("09/29/2022");
-create_new_record("09/30/2022");
-
-// Week 4
-create_new_record("10/03/2022", 6);
-create_new_record("10/04/2022", 5);
-create_new_record("10/04/2022", 4);
-create_new_record("10/06/2022", 3);
-create_new_record("10/07/2022", 2);
+// WEEK 3
+// create_new_record("28/11/2022");
+// create_new_record("29/11/2022");
+// create_new_record("30/11/2022");
 
 export default total_classes_per_day;
